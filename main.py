a4ff9debc963331fa941b662885f243a452121ce
johan932 <jophan.estiven2289@gmail.com>
2026-03-10 22:58:26 -0500

    validacion de credenciales

---
+++ main.py
@@ -0,0 +1,11 @@
+user= "admin@iniremigton.edu.co"
+password = "admin123"
+
+user_input = input("ingrese el nombre de usuario")
+password_input = input("ingrese la contraseña")
+
+if user == user_input and password == password_input:
+    print("credenciales correctas")
+else:
+    print("las credenciales son incorrectas")
+    
